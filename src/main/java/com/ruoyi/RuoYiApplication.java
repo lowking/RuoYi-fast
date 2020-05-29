0991c92d4a01ad22efab8687b31c45a6707f4966
lowking <lowking@sina.cn>
2020-05-29 09:54:48 +0800

    添加依赖包，启动输出pid

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -3,6 +3,7 @@ package com.ruoyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 /**
  * 启动程序
@@ -15,7 +16,9 @@ public class RuoYiApplication
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
-        SpringApplication.run(RuoYiApplication.class, args);
+        SpringApplication application = new SpringApplication(RuoYiApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
